09ccb49023c2b684afd339e2a26848a93b593acc
Luis <lvas248@gmail.com>
2022-12-13 01:22:55 -0500

    create signup functionality in userscontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  private
+
+  def render_invalid(invalid)
+    render json: { errors: invalid.record.errors.full_messages }
+  end
+
 end
